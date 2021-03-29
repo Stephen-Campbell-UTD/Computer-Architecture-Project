97a83d5840fc1ecfa0a53ba93c434c33251e3662
Stephen <kingiskey@gmal.com>
2021-03-29 18:18:41 -0500

    Checks on Register File

--- components/Register File/RegisterFile.v
+++ components/Register File/RegisterFile.v
@@ -1,35 +1,42 @@
-module RAM (
-    address,
+module RegisterFile (
+    selA,
+    selB,
+    selWrite,
+    writeIn,
     isReading,
     clk,
-    data
+    outA,
+    outB
 );
-  parameter ADDRESS_SIZE = 11;
-  parameter MEM_DEPTH = 2 ** ADDRESS_SIZE;
+
+  //Parameters and Ports
+  parameter REG_ADDRESS_SIZE = 2;
+  parameter NUM_REG = 2 ** REG_ADDRESS_SIZE;
   parameter MEM_WORD_SIZE = 64;
 
-  input [ADDRESS_SIZE-1:0] address;
+  input [REG_ADDRESS_SIZE-1:0] selA;
+  input [REG_ADDRESS_SIZE-1:0] selB;
+  input [REG_ADDRESS_SIZE-1:0] selWrite;
+  input [MEM_WORD_SIZE-1:0] writeIn;
   input isReading;
   input clk;
-  inout [MEM_WORD_SIZE-1:0] data;
+  output [MEM_WORD_SIZE-1:0] outA;
+  output [MEM_WORD_SIZE-1:0] outB;
 
-  // NUM_ADDRESS x DATA_BUS_WIDTH bits
-  reg [MEM_WORD_SIZE-1:0] ram_memory[MEM_DEPTH-1:0];
-  reg [MEM_WORD_SIZE-1:0] data_private;
+  //Logic
 
-  initial begin : mem_initialization
-    integer i;
-    for (i = 0; i < MEM_DEPTH; i = i + 1) begin
-      ram_memory[i] = {MEM_WORD_SIZE{1'b0}};
+  reg [MEM_WORD_SIZE-1:0] registers[NUM_REG-1:0];
+  assign outA = registers[selA];
+
+  initial begin : initialization
+  integer i;
+    for(i = 0; i < NUM_REG; i = i+1) begin
+      registers[i] = {MEM_WORD_SIZE{1'b0}};
     end
-    data_private = {MEM_WORD_SIZE{1'b0}};
+    registers[2'b00] = {32{2'b10}};
   end
 
-  always @(posedge clk) begin
-    if (isReading) data_private <= ram_memory[address];
-    else ram_memory[address] <= data;
-  end
 
-  assign data = (isReading ? data_private : {MEM_WORD_SIZE{1'bZ}});
+
 
 endmodule
--- components/Register File/RegisterFile_tb.v
+++ components/Register File/RegisterFile_tb.v
@@ -4,67 +4,34 @@
 module testbench ();
 
   reg clk;
-  wire [0:63] data;
-  reg [0:63] driver;
-  reg isReading;
-  reg [10:0] address;
+  wire [63:0] outA;
+  wire [63:0] outB;
 
-  integer c;
-
-  assign data = isReading ? 64'bZ : driver;
+  RegisterFile mem (
+      .clk(clk),
+      .selA(2'b00),
+      .selB(2'b00),
+      .selWrite(2'b00),
+      .writeIn(64'b0),
+      .isReading(1'b1),
+      .outA(outA),
+      .outB(outB)
+  );
 
   initial begin
     $dumpfile("./build/main.vcd");
     $dumpvars(0, testbench);
-    for (c = 0; c < 2047; c = c + 1) begin
-      $dumpvars(0, mem.ram_memory[c]);
-    end
   end
-  initial begin
-    // Initialize Inputs
-    address   = 0;
-    isReading = 0;
-    clk       = 0;
-    driver    = 0;
-    // Wait 100 ns for global reset to finish
-    #100;
 
-    // Now perform a write at address
-    for (c = 0; c <= 1; c = c + 1) begin
-      clk     <= c;
-      address <= 11'd1024;
-      driver  <= 64'hff04;
-      isReading = 0;
-      #100;
+  initial begin : initialization
+    integer i;
+    for( i = 0; i <= 10; i = i + 1) begin
+      clk <= i;
+      #10;
     end
-
-    isReading = 1;
-
-    // Reads Data from the address right before the one we want
-    for (c = 0; c <= 5; c = c + 1) begin
-      clk     <= c;
-      address <= 11'd1023;
-      #100;
-    end
-
-    // Now perform a read on the address we wrote too
-    for (c = 0; c <= 1; c = c + 1) begin
-      clk     <= c;
-      address <= 11'd1024;
-      #100;
-    end
-
     $finish;
   end
 
-  // always #5 clk = !clk;
 
-  // dff flip (.d(sig),.clk(clk),.reset(resetSIG), .q(qout));
-  RAM mem (
-      .address(address),
-      .isReading(isReading),
-      .clk(clk),
-      .data(data)
-  );
 
 endmodule
